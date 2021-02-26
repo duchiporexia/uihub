5b0d675d3c3ad639c5775d926194fe7ed843fae3
David Xia <duchiporexia@gmail.com>
2021-02-26 13:08:33 +1100

    change eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-multiple-empty-lines': 'off',
+    'no-console': 'off',
+    'object-curly-newline': 'off',
+    'function-paren-newline': 'off',
+    'prefer-const': 'error',
+    "no-unused-vars": 'off',
+  },
   extends: '@react-native-community',
 };
